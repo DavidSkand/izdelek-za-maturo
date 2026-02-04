55bfaa63f822b87703ed5ebd1734d4915ea9d45a
DavidSkander <david.skander8@gmail.com>
2026-02-04 21:42:10 +0100

    Integrate Arduino communication and change camera index
    Added serial communication with Arduino and updated camera index.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import cv2
 from pyapriltags import Detector
 import time
+import serial
 
-camera = cv2.VideoCapture(1) 
+camera = cv2.VideoCapture(0) 
 
 at_detector = Detector()
 
+start_time = time.time()  # Start the timer
+
+arduino = serial.Serial(port='COM4',   baudrate=115200, timeout=.1)
 
 #preveri če kamera deluje
 if not camera.isOpened:  
     print('cannot open camera')
     exit()
 
-start_time = time.time()  # Start the timer
+
+def write_read(x):
+    arduino.write(bytes(x,   'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return  data
+
+
+
 
 while(camera.isOpened()):
     ret, frame = camera.read()       #pridobi ret, Width,Height
@@ -23,7 +35,7 @@ while(camera.isOpened()):
 
     if ret == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
-        tags = at_detector.detect(gray)   #lista tags
+        tags = at_detector.detect(gray)   #lista tagsx
 
         #vzame listo in kliče vsak tag posebaj
         for tag in tags: 
@@ -37,6 +49,12 @@ while(camera.isOpened()):
             cv2.putText(frame, f"ID: {tagid}", text_position, cv2.FONT_HERSHEY_SIMPLEX, (1.5), (0,0,255), 2 )
             xy_position = f"X: {int(center[0])}, Y: {int(center[1])}"
             cv2.putText(frame, xy_position, (bottom_right[0], bottom_right[1] + 20),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2)
+            arduino.write(f"{int(center[0])}\n".encode())
+            resp = arduino.readline().decode(errors="ignore").strip()
+            if resp:
+                print("arduino:", resp)
+            print(center[0])
+
 
         elapsed_time = time.time() - start_time  
         fps = 1 / elapsed_time  # FPS = 1 / Time per frame
