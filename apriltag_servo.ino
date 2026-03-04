874cc81dd924f6ba69d85704f517a38f811afbe4
DavidSkander <david.skander8@gmail.com>
2026-03-04 23:38:25 +0100

    Enhance servo control with target coordinates and smoothing
    Added target coordinates and smoothing for servo control.

--- apriltag_servo.ino
+++ apriltag_servo.ino
@@ -8,11 +8,18 @@ Servo myservo1; // Tilt (Y os)
 float trenutni_kot = 90.0;
 float trenutni_kot1 = 90.0;
 
+float cilj_X = 90.0;
+float cilj_Y = 90.0;
+
+
 // Kalibracija - prilagodi te številke, da laser zadane robove kamere
 int pan_min = 160;   // Kot ko je X = 0
 int pan_max = 20;    // Kot ko je X = 640
 int tilt_min = 160;   // Kot ko je Y = 480
 int tilt_max = 20;  // Kot ko je Y = 0
+int prejsnji_x = 0;
+int prejsnji_y = 0;
+int deadband = 5;
 
 int centerX = 320;  // center 640px wide camera
 int centerY = 240;  // center 480px high camera
@@ -51,27 +58,32 @@ void loop() {
         return; 
       }
 
-      // 3. debug
-      Serial.print("Received X: "); Serial.print(x_pixel);
-      Serial.print(" Y: "); Serial.println(y_pixel);
+      if (abs(x_pixel - prejsnji_x) > deadband || abs(y_pixel - prejsnji_y) > deadband) {
 
-      // 4. (Mapping)
-      // Slika (0-640) se preslika direktno v kote servo.
-      float cilj_X = map(x_pixel, 0, 640, pan_min, pan_max);
-      float cilj_Y = map(y_pixel, 0, 480, tilt_min, tilt_max);
+          cilj_X = map(x_pixel, 0, 640, pan_min, pan_max);
+          cilj_Y = map(y_pixel, 0, 480, tilt_min, tilt_max);
+          
+          // Shrani trenutne piksle za naslednjo primerjavo
+          prejsnji_x = x_pixel;
+          prejsnji_y = y_pixel;
+      } 
 
-      // 5. NIZKOPREPUSTNI FILTER (Smoothing)
+      // spremenimo hitrost 
       
       trenutni_kot = (cilj_X * smoothing) + (trenutni_kot * (1.0 - smoothing));
       trenutni_kot1 = (cilj_Y * smoothing) + (trenutni_kot1 * (1.0 - smoothing));
 
-      // 6. Omejimo kot na varno območje 
+      // Omejimo kot na varno območje 
       trenutni_kot = constrain(trenutni_kot, 10, 170);
       trenutni_kot1 = constrain(trenutni_kot1, 10, 170);
 
-      // 7. Premaknemo servote
+      //Premaknemo servote
       myservo.write((int)trenutni_kot);
       myservo1.write((int)trenutni_kot1);
+
+      // debug
+      Serial.print("Received X: "); Serial.print(x_pixel);
+      Serial.print(" Y: "); Serial.println(y_pixel);
     }
   }
 }
